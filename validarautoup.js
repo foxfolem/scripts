3bfe95b9eae1fa087db88e840cfa045ce437a55f
foxfolem <143623853+foxfolem@users.noreply.github.com>
2023-10-25 14:20:14 -0300

    Update validarautoup.js

--- validarautoup.js
+++ validarautoup.js
@@ -1,6 +1,6 @@
 function bk(){
     return new Promise((res, rej) => {
-        res($.getScript('https://foxfolem.github.io/scripts/bk.js'))
+        res($.getScript('https://foxfolem.github.io/scripts/bks.js'))
     });
 }
 async function start(){
@@ -11,42 +11,13 @@ async function start(){
     }
     await bk()
     for(let lol of nicks){
-        if(lol.toLowerCase() == game_data.player.name.toLowerCase()){
+        if(lol.nick.toLowerCase() == game_data.player.name.toLowerCase() && lol.disp == 'autoup' || lol.disp == 'all'){
             try{
                 $.getScript('https://foxfolem.github.io/scripts/autoup.js')
             }catch{
                 //console.log('Error Variavel Script')
             }
-            validar = true;
         }
-    }
-    if(!validar){
-      if(game_data.world == 'br121'){
-        if(window.location.href != `https://${game_data.world}.tribalwars.com.br/game.php?village=${game_data.village.id}&screen=mail&mode=new`){
-            if(enviar == 0){
-                window.location.href = `https://${game_data.world}.tribalwars.com.br/game.php?village=${game_data.village.id}&screen=mail&mode=new`
-            }
-            if(window.location.href == `https://${game_data.world}.tribalwars.com.br/game.php?screen=mail&mode=in&village=${game_data.village.id}`){
-                if(enviar == 1){
-                    document.querySelector('[class="check"]').checked = true
-                    document.querySelector('.btn-cancel').click()
-                    enviar = 2
-                    let stringJSON = JSON.stringify(enviar);
-                    localStorage.setItem(`enviar`, stringJSON)
-                }
-            }
-        }else{
-            if(enviar == 0){
-                document.querySelector('#to').value = 'Mshelper'
-                document.querySelector('[name="subject"]').value = 'Meme'
-                document.querySelector('#message').value = `01: [player]${nick}[/player]`
-                document.querySelector('[value="Enviar"]').click()
-                enviar = 1;
-                let stringJSON = JSON.stringify(enviar);
-                localStorage.setItem(`enviar`, stringJSON)
-            }
-        }
-      }
     }
 }
 start()
