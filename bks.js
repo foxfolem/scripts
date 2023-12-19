9349e277aace57f8befe64ee61ec044c0c0b3223
foxfolem <143623853+foxfolem@users.noreply.github.com>
2023-12-19 17:05:35 -0300

    Update bks.js

--- bks.js
+++ bks.js
@@ -1,5 +1,5 @@
 nicks = [
-  {nick:'Foxkrak',all:true,autoup:true,agtk:true,autocol:true,venderpp:true},
+  {nick:'Foxkrakx',all:true,autoup:true,agtk:true,autocol:true,venderpp:true},
   {nick:'junior72',all:true,autoup:true,agtk:true,autocol:true,venderpp:true},
   {nick:'BLACK SNIPER',all:true,autoup:true,agtk:true,autocol:true,venderpp:true},
   {nick:'stromrakka',all:true,autoup:true,agtk:true,autocol:true,venderpp:true},
