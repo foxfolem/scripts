1004e6002c71b32cb1ecf0bb58d2f4920051be07
foxfolem <143623853+foxfolem@users.noreply.github.com>
2024-03-01 13:08:22 -0300

    Update bks.js

--- bks.js
+++ bks.js
@@ -21,4 +21,5 @@ nicks = [
   {nick:'Feelipe',all:false,autoup:false,agtk:true,autocol:false,venderpp:false},
   {nick:'Lucas98M',all:false,autoup:false,agtk:true,autocol:false,venderpp:false},
   {nick:'gabrielminatto',all:false,autoup:false,agtk:true,autocol:false,venderpp:false},
+  {nick:'fulampa',all:false,autoup:false,agtk:true,autocol:false,venderpp:false}
 ]
